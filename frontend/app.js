80712876d52ae9d74cdae141d1743b8f7d95fa06
greyTurtleDove <radek29c@gmail.com>
2026-05-27 16:44:26 +0200

    feat/shop-frontend-basics

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+const baseAddress = "http://127.0.0.1:8000";
+
+function openShop() {
+	window.location.replace(baseAddress + "//shop.html");
+}
+
+function openMainPage() {
+	window.location.replace(baseAddress);
+}
