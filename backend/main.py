03c0f8bf7184a37e649281b35198a8a22544d6f4
greyTurtleDove <radek29c@gmail.com>
2026-06-01 19:47:35 +0200

    feat/cors

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from backend.db.session import init_db
 from backend.api.router import router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +12,17 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Cukiernia Clicker", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
